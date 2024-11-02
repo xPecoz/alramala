5eb8a887eaa500fe68398394803e824af57e6704
xPecoz <abdulrahmansalam376@gmail.com>
2024-11-02 12:40:48 +0200

    layout and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo1 from "../imgs/logo 2.png";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header>
+      <div className="container">
+        <div className="logo">
+          <img src={logo1} alt="Logo"></img>
+        </div>
+        <ul className="links">
+          <li>
+            <Link to="#">Home</Link>
+          </li>
+          <li>
+            <Link to="#">Projects</Link>
+          </li>
+          <li>
+            <Link to="#">About As</Link>
+          </li>
+          <li>
+            <Link to="#">Join Us</Link>
+          </li>
+          <li>
+            <Link to="#">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <main className="home">
+      <section className=""></section>
+    </main>
+  );
 };
 
 export default Home;
